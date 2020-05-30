35f725463fbb50288500c96d8e3408c2d4ff21fe
vik625 <vik625singh@gmail.com>
2020-05-30 20:49:50 +0530

    Updates

---
+++ precache-manifest.24da0a56aca947f0435973fa582727b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e31639c6508de0dd5fbae95e203de4",
+    "url": "/Mobishaala/index.html"
+  },
+  {
+    "revision": "1230493cf927861bb29b",
+    "url": "/Mobishaala/static/css/main.c05369bb.chunk.css"
+  },
+  {
+    "revision": "2dd613ebbc1c07d2fa0b",
+    "url": "/Mobishaala/static/js/2.cc9781a0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Mobishaala/static/js/2.cc9781a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1230493cf927861bb29b",
+    "url": "/Mobishaala/static/js/main.f1e279f8.chunk.js"
+  },
+  {
+    "revision": "2a0d614bf85d61df2c3a",
+    "url": "/Mobishaala/static/js/runtime-main.dc3923b7.js"
+  }
+]);
